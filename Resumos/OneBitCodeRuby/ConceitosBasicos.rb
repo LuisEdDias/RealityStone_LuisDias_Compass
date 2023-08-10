81627747cdc6449a667b5b35bd8e8c8930b23b91
LuisDiasCompass <luis.dias.pb@compasso.com.br>
2023-08-10 15:26:22 -0300

    Create file ConceitosBasicos.rb

---
+++ Resumos/OneBitCodeRuby/ConceitosBasicos.rb
@@ -0,0 +1,26 @@
+inteiro = 1
+puts inteiro.class 
+
+float = 1.1
+puts float.class
+
+string = "Hello World"
+puts string.class
+
+boolean = true
+puts boolean.class
+
+boolean = false
+puts boolean.class
+
+array = ["position_0", "position_1", "position_2"]
+puts array.class
+
+hash = {key_1: 'value1', key_2: 'value2', key_3: 'value3'}
+puts hash.class
+
+symbol = :symbol
+puts symbol.class
+
+range = 1..10
+puts range.class
