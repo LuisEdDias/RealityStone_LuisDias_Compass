91e8382029c97d708bd379427835b3560be8e3fc
LuisDiasCompass <luis.dias.pb@compasso.com.br>
2023-08-11 17:13:56 -0300

    Create file EntradaSaida.rb

---
+++ Resumos/OneBitCodeRuby/EntradaSaida.rb
@@ -0,0 +1,11 @@
+#Saída de dados
+print "Saída sem quebra de linha"
+
+puts "Saída com quebra de linha"
+puts "Digite algo:"
+
+#Entrada de dados
+data = gets.chomp  
+
+#Saída de dados
+print "Você digitou -> #{data}"
