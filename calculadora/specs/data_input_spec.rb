25de01e93aa09f1d8fb2cb738c5b00b9ff6eb233
LuisDiasCompass <luis.dias.pb@compasso.com.br>
2023-08-19 15:25:03 -0300

    data_input_spec.rb updated

--- calculadora/specs/data_input_spec.rb
+++ calculadora/specs/data_input_spec.rb
@@ -6,27 +6,27 @@ describe 'Class Input -> input_option' do
         @input = Input.new
     end
 
-    it 'returns 1 for input "1"' do
+    it 'should return 1 for input "1"' do
         expect(@input.input_option '1').to eq 1
     end
   
-    it 'returns 0 for input "0"' do
+    it 'should return 0 for input "0"' do
         expect(@input.input_option '0').to eq 0
     end
     
-    it 'returns false for negative numbers' do
+    it 'should return false for negative numbers' do
         expect(@input.input_option '-1').to eq false
     end
 
-    it 'returns false for letters' do
+    it 'should return false for letters' do
         expect(@input.input_option 'invalid').to eq false
     end
 
-    it 'returns false for symbols' do
+    it 'should return false for symbols' do
         expect(@input.input_option '/\<>[]~{}^').to eq false
     end
 
-    it 'returns false for nil' do
+    it 'should return false for nil' do
         expect(@input.input_option '').to eq false
     end
 end
@@ -36,27 +36,27 @@ describe 'Class Input -> input_float' do
         @input = Input.new
     end
 
-    it 'returns a float for string integer' do
+    it 'should return a float for string integer' do
         expect(@input.input_float '5').to eq 5.0
     end
 
-    it 'returns a float for string float' do
+    it 'should return a float for string float' do
         expect(@input.input_float '3.14').to eq 3.14
     end
 
-    it 'returns a negative float for negative string number' do
+    it 'should return a negative float for negative string number' do
         expect(@input.input_float '-5').to eq -5.0
     end
     
-    it 'returns false for letters' do
+    it 'should return false for letters' do
         expect(@input.input_float 'invalid').to eq false
     end
     
-    it 'returns false for symbols' do
+    it 'should return false for symbols' do
         expect(@input.input_float '/\<>[]~{}^').to eq false
     end
 
-    it 'returns false for nil' do
+    it 'should return false for nil' do
         expect(@input.input_float '').to eq false
     end
 end
