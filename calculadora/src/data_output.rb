d4980c1610ece1b2edd5c55c22bbefef7438d54d
LuisDiasCompass <luis.dias.pb@compasso.com.br>
2023-08-19 13:22:03 -0300

    data_output.rb refactoreted

--- calculadora/src/data_output.rb
+++ calculadora/src/data_output.rb
@@ -1,96 +1,138 @@
 # Handles data output.
 class Output
+    attr_accessor :message
+
     # Prints the header.
     def output_head
-        puts "*****************************************************************"
-        puts "---------------------------CALCULADORA---------------------------"
+        @message = "*****************************************************************\n" +
+                   "---------------------------CALCULADORA---------------------------\n"
+        print @message
     end
 
     # Prints the start message.
     def output_start
-        puts "*****************************************************************"
-        puts "Deseja fazer uma operação matemática? Digite 1 - SIM ou 0 - NÃO"
-        print "-> "
+        @message = (
+            "*****************************************************************\n" +
+            "Deseja fazer uma operação matemática? Digite 1 - SIM ou 0 - NÃO\n" +
+            "-> "
+        )
+        print @message
     end
 
     # Ask for a number.
     def first_number
-        puts "*****************************************************************"
-        puts "Digite o primeiro número:"
-        print "-> "
+        @message = (
+            "*****************************************************************\n" +
+            "Digite o primeiro número:\n" +
+            "-> "
+        )
+        print @message
     end
 
     # Prints the menu.
     def output_menu(calculate_string)
-        puts "Seu calculo até agora -> (#{ calculate_string })"
-        puts "*****************************************************************"
-        puts "Selecione a operação:"
-        puts "1 - SOMA"
-        puts "2 - SUBTRAÇÃO"
-        puts "3 - MULTIPLICAÇÃO"
-        puts "4 - DIVISÃO"
-        puts "5 - APAGAR ÚLTIMA OPERAÇÃO"
-        puts "0 - RESULTADO"
-        print "-> "
+        @message = (
+            "Seu calculo até agora -> (#{ calculate_string })\n" +
+            "*****************************************************************\n" +
+            "Selecione a operação:\n" +
+            "1 - SOMA\n" +
+            "2 - SUBTRAÇÃO\n" +
+            "3 - MULTIPLICAÇÃO\n" +
+            "4 - DIVISÃO\n" +
+            "5 - APAGAR ÚLTIMA OPERAÇÃO\n" +
+            "0 - RESULTADO\n" +
+            "-> "
+        )
+        print @message
     end
 
     # Ask for the next number.
     def next_number
-        puts "*****************************************************************"
-        puts "Digite o próximo número:"
-        print "-> "
+        @message = (
+            "*****************************************************************\n" +
+            "Digite o próximo número:\n" +
+            "-> "
+        )
+        print @message
     end
 
     # Prints error message for invalid operation.
     def invalid_operation
-        puts "*****************************************************************"
-        puts "Operação inválida! Tente novamente."
+        @message = (
+            "*****************************************************************\n" +
+            "Operação inválida! Tente novamente.\n"
+        )
+        print @message
     end 
 
     # Prints error message for invalid option.
     def invalid_option
-        puts "*****************************************************************"
-        puts "Opção inválida! Tente novamente."
+        @message = (
+            "*****************************************************************\n" +
+            "Opção inválida! Tente novamente.\n"
+        )
+        print @message   
     end 
 
     # Prints error message for invalid number.
     def invalid_number
-        puts "*****************************************************************"
-        puts "Número inválido! Tente novamente."
+        @message = (
+            "*****************************************************************\n" +
+            "Número inválido! Tente novamente.\n"
+        )
+        print @message  
     end 
 
     # Prints error message for division by zero.
     def division_zero
-        puts "Não é possível dividir por zero! Tente novamente."
+        @message = (
+            "Não é possível dividir por zero! Tente novamente.\n"
+        )
+        print @message
     end
 
     # Prints message to confirme delete.
     def delete_confirme
-        puts "*****************************************************************"
-        puts "Você tem certeza que quer apagar a última operação? Digite 1 - SIM ou 0 - NÃO"
-        print "-> "
+        @message = (
+            "*****************************************************************\n" +
+            "Você tem certeza que quer apagar a última operação? Digite 1 - SIM ou 0 - NÃO\n" +
+            "-> "
+        )
+        print @message   
     end
 
     # Prints message for delete canceled.
     def delete_canceled
-        puts "Nada foi apagado!"
+        @message = (
+            "Nada foi apagado!\n"
+        )
+        print @message
     end
 
     # Prints error message for delete.
     def delete_error
-        puts "Não há nada para apagar!"
+        @message = (
+            "Não há nada para apagar!\n"
+        )
+        print @message
     end
 
     # Prints message for result.
     def message_result(calculate_string)
-        puts "*****************************************************************"
-        print "O resultado para (#{ calculate_string }) é -> "
+        @message = (
+            "*****************************************************************\n" +
+            "O resultado para (#{ calculate_string }) é -> "
+        )
+        print @message 
     end
 
     # Prints the final message.
     def output_exit
-        puts "*****************************************************************"
-        puts "Até logo!"
-        puts "*****************************************************************"
+        @message = (
+            "*****************************************************************\n" +
+            "Até logo!\n" +
+            "*****************************************************************\n"
+        )
+        print @message
     end
 end
